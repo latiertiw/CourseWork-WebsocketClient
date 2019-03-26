95ee140238d02f5e5d76ff35539dad1f393dbb9b
latiertiw <latierti@yandex.ru>
2019-03-26 23:01:34 +0300

    feat:add goals registration + style changes

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ var socket = new WebSocket("ws://localhost:8080/project_war_exploded/my");
               if(packet.key=="login_admin"){
                 alert("Logined as admin");
                 $(".loginBlock").toggle(300);
-                $("main .menuBlock").toggle(300);
+                $("main .menuBlock").slideToggle(300);
                 $("main .adminMenu").toggle(300);
                 $(" .menuAutorisationStatus").text(()=>{return packet.data})
                 $(" .menuName").text(()=>{return "МЕНЮ АДМИНИСТРАТОРА"})
@@ -51,6 +51,14 @@ var socket = new WebSocket("ws://localhost:8080/project_war_exploded/my");
                 state.loginedName = `${packet.data}`;
                 state.openedMenu = "user_menu"
               }
+              if(packet.key=="successful_add_goal"){
+                alert("Цель успешно добавлена");
+                exitToMenu();
+              }
+              if(packet.key=="goals_data"){
+                let data = JSON.parse(packet.data);
+                console.log(data)
+              }
               if(packet.key=="wrong_login_data"){
                 alert("Неправильное имя пользователя или пароль");
               }
@@ -63,7 +71,7 @@ var socket = new WebSocket("ws://localhost:8080/project_war_exploded/my");
               }
           };
 
-
+         
           socket.onerror = function(error) {
             document.querySelector('.wrapper .statusBlock').innerHTML='ERROR'
           };
@@ -75,33 +83,51 @@ var socket = new WebSocket("ws://localhost:8080/project_war_exploded/my");
       $("main .consoleBlock").toggle(0);
       $("main .registrationBlock").toggle(0);
       $("main .menuBlock").toggle(0);
+      $("main .addGoalMenu").toggle(0);
       $("main .adminMenu").toggle(0);
       $("main .userMenu").toggle(0);
-      $(" .wrapper").toggle(0);
-      $(" .wrapper").toggle(600);
+      //$(" .wrapper").fadeToggle(0);
+      //$(" .wrapper").fadeToggle(000);
     }
     
 
   function backToLogin(){  // Выход из блока регистрации
-      $("main .registrationBlock").toggle(200);
-      $("main .loginBlock").toggle(200);
-      $(":password").css("border","2px solid rgba(0, 0, 0, 0.424)");
+    $("#loginInput").attr('value',"");
+    $("#passwordInput").attr('value',"");
+
+    $("main .registrationBlock").toggle(200);
+    $("main .loginBlock").toggle(200);
+    $(":password").css("border","2px solid rgba(0, 0, 0, 0.424)");
   }
 
   function exitFromMenu(){ // Выход из рабочего меню в меню авторизации
+
+      $("#loginInput").attr('value',"");
+      $("#passwordInput").attr('value',"")
+
       $(".loginBlock").slideToggle(400);
       $("main .menuBlock").slideToggle(150);
       setTimeout(()=>{
-        if(state.loginStatus=="admin"){$("main .adminMenu").toggle(200);}
-        else $("main .userMenu").toggle(200);
+        if(state.loginStatus=="admin"){$("main .adminMenu").toggle(0);}
+        else if(state.loginStatus=="user") $("main .userMenu").toggle(0);
         $(" .menuAutorisationStatus").text(()=>{return "Не авторизирован"})
         $(" .menuName").text(()=>{return "Не авторизирован"})
+        state.loginStatus = null;
+        state.loginedName = null;
       },450)
-      state.loginStatus = null;
-      state.loginedName = null;
-      state.openedMenu = "main"
+     
+      state.openedMenu = "main";
   }
 
+  function exitToMenu(){
+    if(state.loginStatus=="admin"){$("main .adminMenu").toggle(200);}
+    else $("main .userMenu").toggle(200);
+
+    if(state.openedMenu=="add_goal_menu"){
+      $("main .addGoalMenu").toggle(200);
+      state.openedMenu="main";
+    }
+  }
 
 
 
@@ -164,10 +190,10 @@ var socket = new WebSocket("ws://localhost:8080/project_war_exploded/my");
 
       if(data.password==data.passwordRepeat){
         $(":password").css("border","2px solid rgba(0, 0, 0, 0.425)");
-       data = JSON.stringify(data);
-       obj.data=data;
-       obj = JSON.stringify(obj);
-       socket.send(obj);
+        data = JSON.stringify(data);
+        obj.data=data;
+        obj = JSON.stringify(obj);
+        socket.send(obj);
       }
       else{
         $(":password").css("border","2px solid rgb(197, 112, 112)");
@@ -177,7 +203,49 @@ var socket = new WebSocket("ws://localhost:8080/project_war_exploded/my");
        
       }
   }
+
+  function sendNewGoal() {
+      let obj = {}
+      let data ={}
+      obj.key = 'add_goal';
+
+      data.number = $(".addGoalMenu .inputContainer .inputs").children().eq(0).attr('value');
+      data.name = $(".addGoalMenu .inputContainer .inputs").children().eq(1).attr('value');
+      data.legend = $(".addGoalMenu .inputContainer .inputs").children().eq(2).attr('value');
+      data.cost_opt = $(".addGoalMenu .inputContainer .inputs").children().eq(3).attr('value');
+      data.cost_per_kg = $(".addGoalMenu .inputContainer .inputs").children().eq(4).attr('value');
+      data.count = $(".addGoalMenu .inputContainer .inputs").children().eq(5).attr('value');
      
+       data = JSON.stringify(data);
+       obj.data=data;
+       obj = JSON.stringify(obj);
+       socket.send(obj);
+      
+  }
+
+  function sendGoalsRequest() {
+    let obj = {}
+    let data = {}
+    obj.key = 'goal_request';
+   
+
+    data = JSON.stringify(data);
+    obj.data=data;
+    obj = JSON.stringify(obj);
+    socket.send(obj);
+  }
+     
+
+
+
+  // ADMINMENUMETHODS
+
+  function toggleAddGoalMenu(){
+    if(state.loginStatus=="admin"){$("main .adminMenu").toggle(200);}
+        else $("main .userMenu").toggle(200);
+    $("main .addGoalMenu").toggle(200);
+    state.openedMenu="add_goal_menu";
+  }
 
      
 
---
+++ input.js
@@ -0,0 +1,23 @@
+
+
+
+function isNumberKey(evt)
+       {
+          var charCode = (evt.which) ? evt.which : evt.keyCode;
+          if (charCode != 46 && charCode > 31 
+            && (charCode < 48 || charCode > 57))
+             return false;
+
+          return true;
+       }
+
+       
+var validNumber = new RegExp(/^\d*\.?\d*$/);
+let lastValid="";
+function validateNumber(elem) {
+  if (validNumber.test(elem.value)) {
+    lastValid = elem.value;
+  } else {
+    elem.value = lastValid;
+  }
+}
